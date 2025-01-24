487f1a2c4c36f5a218c550c72ce5033fc3e87d3c
Your Name <youremail@example.com>
2025-01-24 17:16:13 +0200

    developed components

--- myquiz/src/App.jsx
+++ myquiz/src/App.jsx
@@ -1,8 +1,20 @@
-export default function App () {
+import { Highscore } from "./components/Highscore";
+import { PlayButton } from "./components/PlayButton";
 
+export default function App () {
+  // Variables definition
+  let highscore = 0;
+
+  // Functions
+  function getHighscore() {
+    return highscore;
+  }
+
+  // Logic
   return (
-    <>
-     
-    </>
+    <div className="menu-container">
+     <PlayButton/>
+     <Highscore highscore={getHighscore()}/>
+    </div>
   )
 }
---
+++ myquiz/src/components/PlayButton.jsx
@@ -0,0 +1,10 @@
+import "../styles/App.css"
+
+export function PlayButton() {
+
+  return (
+    <button className = "btn">
+      Start Game!
+    </button>
+  )
+}
